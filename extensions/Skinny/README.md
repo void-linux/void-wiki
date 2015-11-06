9e307303906a2a414a363264488a392f34124f24
Greg Fitzgerald <greg@gregf.org>
2015-11-06 10:32:49 -0500

    Remove some unused extensions

--- extensions/Skinny/README.md
+++
@@ -1,4 +0,0 @@
-Skinny
-======
-
-Skinny is a set of tools to make skinning MediaWiki more bearable.
--- extensions/Skinny/templates/breadcrumbs.tpl.php
+++
@@ -1,9 +0,0 @@
-<nav class="breadcrumbs" id="main-breadcrumbs">
-<?php foreach($trees as $tree): ?>
-	<ul>
-	<?php foreach($tree as $cat): ?>
-		<li><?php echo $cat; ?>
-	<?php endforeach; ?>
-	</ul>
-<?php endforeach; ?>
-</nav>
